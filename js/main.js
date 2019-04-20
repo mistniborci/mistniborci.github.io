5b6d791adcc0be81930968eb96e42166da028952
Martin Sznapka <bigmartin@outlook.com>
2019-04-20 16:10:49 +0200

    Show only "O nas"

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ angular
                 templateUrl: 'views/data.html'
             })
             .otherwise({
-                redirectTo: '/akce'
+                redirectTo: '/onas'
             });
     })
     .constant('constants', {
